ca86c6713098090ac6961b36eb53c37de5b98691
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-04-19 08:58:32 -0300

    FEAT: Iniciando a configuração de rotas

---
+++ ecommerce-frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
